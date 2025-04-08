8853f332b29dac392fd376afd8dfaa2b3d3d0b02
3031249 <3031249@30312-49>
2025-04-08 16:15:43 +0900

    4/8 16:15　ログインとか認証はクリアした

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 --テーブルが存在したら削除する
 DROP TABLE IF EXISTS bookrecordtable;
+DROP TABLE IF EXISTS authentications;
+DROP TYPE IF EXISTS role;
 
 --テーブルの作成
 CREATE TABLE bookrecordtable (
@@ -37,6 +39,16 @@ CREATE TABLE bookrecordtable (
 		
 		--書影(任意)
 		image_url VARCHAR(1000)
+);
 
-	
-)
+-- 権限用のENUM型
+CREATE TYPE role AS ENUM ('ADMIN', 'USER');
+-- 認証情報を格納するテーブル
+CREATE TABLE authentications (
+-- ユーザー名：主キー
+username VARCHAR(50) PRIMARY KEY,
+-- パスワード
+password VARCHAR(255) NOT NULL,
+-- 権限
+authority role NOT NULL
+);
